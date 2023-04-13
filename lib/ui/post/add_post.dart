d0809013a52cf4a65a5e95ef7630461ff92103b7
keyursavani <savanikeyur1011@gmail.com>
2023-04-13 19:29:01 +0530

    final commite

---
+++ lib/ui/post/add_post.dart
@@ -0,0 +1,107 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_tutorial/Utils/utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddPost extends StatefulWidget{
+  const AddPost({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+   return AddPostState();
+  }
+}
+class AddPostState extends State<AddPost>{
+  bool loading = false;
+  final databaseRef = FirebaseDatabase.instance.ref('Post');
+  final postController = TextEditingController();
+  final enabledBorder = const OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(9),
+    ),
+    borderSide: BorderSide(color: Colors.black),
+  );
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Post"),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40,left:20,right: 20,bottom: 20),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 0,right: 0) ,
+              child: TextFormField(
+                controller: postController,
+                cursorColor: Colors.black12,
+                keyboardType: TextInputType.text,
+                style: const TextStyle(fontSize: 16, color: Color(0xFF19232d)),
+                // validator: (value){
+                //   if(value!.isEmpty){
+                //     return "Enter Password";
+                //   }
+                //   return null;
+                // },
+                obscureText: false,
+                maxLines: 4,
+                decoration: InputDecoration(
+                  hintText: "What is in your mind?",
+                  hintStyle: const TextStyle(fontSize:17, color: Colors.black54),
+                  // prefixIcon:const Icon(Icons.lock_outline ,color: Colors.black45,),
+                  enabledBorder: enabledBorder,
+                  border: enabledBorder,
+                  focusedBorder: enabledBorder,
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                ),
+              ),
+            ),
+            const SizedBox(height: 50,),
+            InkWell(
+              onTap: (){
+                setState(() {
+                  loading = true;
+                });
+                databaseRef.child(DateTime.now().microsecondsSinceEpoch.toString()).set({
+                  'id' : DateTime.now().microsecondsSinceEpoch.toString(),
+                  'title': postController.text.toString(),
+                  }).then((value) {
+                    Utils().toastMessage('Post Added');
+                    setState(() {
+                      loading = false;
+                    });
+                }).onError((error, stackTrace) {
+                  Utils().toastMessage(error.toString());
+                  setState(() {
+                    loading = false;
+                  });
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  color: Colors.deepPurple,
+                ),
+                child:  Align(
+                  alignment: Alignment.center,
+                  child: loading ? const CircularProgressIndicator(strokeWidth: 3,color: Colors.white,)
+                      :  const Text("Add",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22
+                  ),),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/post/post_screen.dart
+++ lib/ui/post/post_screen.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_tutorial/Utils/utils.dart';
 import 'package:firebase_tutorial/ui/auth/login_screen.dart';
+import 'package:firebase_tutorial/ui/post/add_post.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +20,8 @@ class PostScreen extends StatefulWidget{
 
 class PostScreenState extends State<PostScreen>{
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref('Post');
+  final searchFilter = TextEditingController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -42,6 +48,68 @@ class PostScreenState extends State<PostScreen>{
           ),
           const SizedBox(width: 10,),
         ],
+      ),
+      body: Column(
+        children:  [
+          // Expanded(
+          //     child: StreamBuilder(
+          //       stream: ref.onValue,
+          //       builder: (context ,AsyncSnapshot<DatabaseEvent> snapshot){
+          //         if(!snapshot.hasData){
+          //           return const CircularProgressIndicator();
+          //         }
+          //         else{
+          //           Map<dynamic,dynamic> map = snapshot.data!.snapshot.value as dynamic;
+          //           List<dynamic> list = [];
+          //           list.clear();
+          //           list = map.values.toList();
+          //           return ListView.builder(
+          //               itemCount: snapshot.data!.snapshot.children.length,
+          //               itemBuilder: (context , index){
+          //                 return ListTile(
+          //                   title: Text(list[index]['title'].toString()),
+          //                   subtitle: Text(list[index]['id'].toString()),
+          //                 );
+          //               });
+          //         }
+          //       },
+          //     )),
+          const SizedBox(height: 20,),
+          Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 10),
+           child: TextFormField(
+             controller: searchFilter,
+             decoration: const InputDecoration(
+               hintText: 'Search',
+               border: OutlineInputBorder(),
+             ),
+           ),
+         ),
+         const SizedBox(height: 20,),
+         Expanded(
+             child: FirebaseAnimatedList(
+               query: ref,
+               defaultChild: const Text("Loading"),
+               itemBuilder: (context , snapshot ,index ,animation){
+                 return  ListTile(
+                   title:  Text(snapshot.child('title').value.toString()),
+                   subtitle: Text(snapshot.child('id').value.toString()),
+                 );
+               },
+             )
+         ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context){
+              return const AddPost();
+            })
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
